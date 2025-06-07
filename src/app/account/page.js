e2cd759456691678b1cabf479ed669a793948668
Dimas Indra Jaya <dimaszxc101@gmail.com>
2025-06-08 04:07:53 +0700

    update

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -8,6 +8,14 @@ export default function AccountPage() {
         <main className="bg-black text-white min-h-screen px-6 md:px-20 py-12 space-y-12">
             <ProfileSection profile={dummyProfile} />
 
+            <h2 className="text-lg font-semibold text-center text-white py-12">
+                Silakan pilih atau klik video yang ingin kamu monitoring.
+                <br className="hidden md:block" />
+                Aplikasi ini membantu mendeteksi dan menyembunyikan komentar
+                **spam**, **kasar**, atau **berbau judi online** secara
+                otomatis.
+            </h2>
+
             <section>
                 <h3 className="text-xl font-bold mb-4">Daftar Video</h3>
                 <div className="flex flex-wrap gap-y-6 justify-center md:justify-between">
