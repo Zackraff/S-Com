07e17d8bdcad34aee284323d4feb4b703663b6ec
Dimas Indra Jaya <dimaszxc101@gmail.com>
2025-06-10 10:29:53 +0700

    update action about

--- src/app/about/components/CallToAction.jsx
+++ src/app/about/components/CallToAction.jsx
@@ -7,7 +7,7 @@ export function CallToAction() {
         <section className="text-center w-full">
             <h4 className="text-2xl font-bold mb-4 ">Tertarik Mencoba?</h4>
             <Link
-                href="/login"
+                href="/account"
                 className="inline-block bg-white text-black font-semibold px-6 py-2 rounded hover:bg-gray-200 transition"
             >
                 Mulai Sekarang
